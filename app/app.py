565aa8a9b0d9a62411d51e9c1619f9ab567aba3d
Daniel Groh <daniel.groh@iti-software.de>
2020-02-15 13:37:27 +0100

    remove category service

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_restful import Api
-from category import Category, CategoryList
 from event import Event, EventList
 
 app = Flask(__name__)
@@ -11,8 +10,6 @@ api = Api(app)
 ##
 api.add_resource(EventList, '/api/v1/events')
 api.add_resource(Event, '/api/v1/events/<string:event_id>')
-api.add_resource(CategoryList, '/api/v1/categories')
-api.add_resource(Category, '/api/v1/categories/<string:category_id>')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
