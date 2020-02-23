a99313de377ae108a7fefd591d25e62b6873d8ee
Daniel Groh <daniel.groh@iti-software.de>
2020-02-23 13:39:24 +0100

    improve project structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
-from app.event import Event, EventList
+
+from resources.event import Event, EventList
 
 app = Flask(__name__)
 api = Api(app)
