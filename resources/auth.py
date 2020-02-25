60246d8faf4126d13303650558f2b2d8c196fd29
Daniel Groh <daniel.groh@iti-software.de>
2020-02-25 21:26:48 +0100

    add jwt token generation

--- resources/auth.py
+++ resources/auth.py
@@ -1,6 +1,9 @@
 from flask import Flask, make_response
 from flask_restful import Resource, reqparse
+from datetime import datetime, timedelta
+import os
 import bcrypt
+import jwt
 
 from . import HttpStatusCode
 from db import mongo
@@ -21,6 +24,18 @@ class Login(Resource):
         user = users.find_one_or_404({'email': args['email']})
 
         if bcrypt.checkpw(args['password'].encode('utf-8'), user['hashed_password']):
-            return make_response({'token': ''}, HttpStatusCode.HTTP_201_CREATED)
+            try:
+                payload = {
+                    'exp': datetime.utcnow() + timedelta(days=1),
+                    'iat': datetime.utcnow(),
+                    'sub': str(user['_id'])
+                }
+                
+                token = jwt.encode(payload, os.getenv('SECRET_KEY'), algorithm='HS256').decode("utf-8")
+
+                return make_response({'token': token}, HttpStatusCode.HTTP_201_CREATED)
+            except Exception as e:
+                # TODO: Log the exception 
+                return '[HTTP_500_INTERNAL_SERVER_ERROR]', HttpStatusCode.HTTP_500_INTERNAL_SERVER_ERROR
 
         return '[HTTP_401_UNAUTHORIZED]', HttpStatusCode.HTTP_401_UNAUTHORIZED
