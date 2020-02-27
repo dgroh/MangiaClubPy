fff7c13840c03fde3330a7bacc2aca97db59572a
Daniel Groh <daniel.groh@iti-software.de>
2020-02-27 23:20:48 +0100

    add logout resource based on redis

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 
 from resources.event import Event, EventList
 from resources.user import User, UserList
-from resources.auth import Login
+from resources.auth import Login, Logout
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,6 +16,7 @@ api.add_resource(Event, '/api/v1/events/<string:id>')
 api.add_resource(UserList, '/api/v1/users')
 api.add_resource(User, '/api/v1/users/<string:id>')
 api.add_resource(Login, '/api/v1/auth/login')
+api.add_resource(Logout, '/api/v1/auth/logout')
 
 if __name__ == '__main__':
     app.run()
--- resources/auth.py
+++ resources/auth.py
@@ -2,12 +2,14 @@ from flask import Flask, make_response, request
 from flask_restful import Resource, reqparse
 from datetime import datetime, timedelta
 from functools import wraps
-import os
-import bcrypt
-import jwt
+from bson import ObjectId
+import jwt, json, bcrypt, uuid, os
 
 from . import HttpStatusCode
-from db import mongo
+from db import mongo, redis
+
+token_audience = '' # TODO: What is the audience?
+token_issuer = '' # TODO: What is the issuer?
 
 def token_required(f):
     @wraps(f)
@@ -21,16 +23,34 @@ def token_required(f):
             return make_response('[HTTP_403_FORBIDDEN]', HttpStatusCode.HTTP_403_FORBIDDEN)         
 
         try:
-            data = jwt.decode(token, os.getenv('SECRET_KEY'))
+            data = jwt.decode(token, os.getenv('SECRET_KEY'), audience=token_audience, issuer=token_issuer, algorithms=['HS256'])
+
+            claim_sub = data['sub']
+            claim_email = data['email']
+
+            user = mongo.db.users.find_one({ '_id': ObjectId(claim_sub), 'email': claim_email })
+
+            if not user:
+                return make_response('[INVALID_TOKEN]', HttpStatusCode.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            user_id = str(user['_id'])
+
+            token_alive = redis.get(user_id)
+
+            if not token_alive:
+                return make_response('[INVALID_TOKEN]', HttpStatusCode.HTTP_500_INTERNAL_SERVER_ERROR)
+
         except Exception as e:
             # TODO: Log the exception 
             return make_response('[INVALID_TOKEN]', HttpStatusCode.HTTP_500_INTERNAL_SERVER_ERROR)         
 
-        return f(self, data['sub'], *args, **kwargs)
+        return f(self, user_id, *args, **kwargs)
 
     return decorated
 
+
 class Login(Resource):
+
     def __init__(self):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('email', required=True)
@@ -45,17 +65,36 @@ class Login(Resource):
 
         if bcrypt.checkpw(args['password'].encode('utf-8'), user['hashed_password']):
             try:
+                user_id = str(user['_id'])
+                
+                expires_in = timedelta(days=60)
+
                 payload = {
-                    'exp': datetime.utcnow() + timedelta(days=1),
+                    'iss': token_issuer,
+                    'aud': token_audience,
+                    'exp': datetime.utcnow() + expires_in,
                     'iat': datetime.utcnow(),
-                    'sub': str(user['_id'])
+                    'sub': user_id,
+                    'email': user['email'],
+                    'jti:': str(uuid.uuid4())
                 }
                 
-                token = jwt.encode(payload, os.getenv('SECRET_KEY'), algorithm='HS256')
+                token = jwt.encode(payload, os.getenv('SECRET_KEY'), algorithm='HS256').decode('utf-8')
 
-                return make_response({'token': token.decode("utf-8")}, HttpStatusCode.HTTP_201_CREATED)
+                redis.setex(user_id, int(expires_in.total_seconds()), token)
+
+                return make_response({'token': token}, HttpStatusCode.HTTP_201_CREATED)
             except Exception as e:
                 # TODO: Log the exception 
                 return make_response('[HTTP_500_INTERNAL_SERVER_ERROR]', HttpStatusCode.HTTP_500_INTERNAL_SERVER_ERROR)         
 
-        return make_response('[HTTP_401_UNAUTHORIZED]', HttpStatusCode.HTTP_401_UNAUTHORIZED) 
+        return make_response('[HTTP_401_UNAUTHORIZED]', HttpStatusCode.HTTP_401_UNAUTHORIZED)
+
+
+class Logout(Resource):
+
+    @token_required
+    def delete(self, user_id):
+        redis.delete(user_id)
+
+        return '[HTTP_204_NO_CONTENT]', HttpStatusCode.HTTP_204_NO_CONTENT
