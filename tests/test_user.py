4d1b422221e1601824a2e4d5532e0ad5f99a2371
Daniel Groh <daniel.groh@iti-software.de>
2020-03-05 17:23:04 +0100

    add test files for event and user

---
+++ tests/test_user.py
@@ -0,0 +1,47 @@
+import unittest
+
+
+# WIP
+class TestUserMethods(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_get_user_not_found(self):
+        pass
+
+    def test_get_user_successful(self):
+        pass
+
+    def test_put_user_without_token(self):
+        pass
+
+    def test_put_user_not_found(self):
+        pass
+
+    def test_put_user_successful(self):
+        pass
+
+
+# WIP
+class TestUserListMethods(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_post_user_without_token(self):
+        pass
+
+    def test_post_user_already_exists(self):
+        pass
+
+    def test_post_user_successful(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
