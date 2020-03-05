558094d61d479a7fc3ba5e182269400b40435c57
Daniel Groh <daniel.groh@iti-software.de>
2020-03-05 11:50:35 +0100

    fix wrong url routes

--- api/resources/__init__.py
+++ api/resources/__init__.py
@@ -8,9 +8,9 @@ from .user import UserList, User
 def init_resources(app):
     api = Api(app)
 
-    api.add_resource(EventList, '/resources/v1/events')
-    api.add_resource(Event, '/resources/v1/events/<string:id>')
-    api.add_resource(UserList, '/resources/v1/users')
-    api.add_resource(User, '/resources/v1/users/<string:id>')
-    api.add_resource(Login, '/resources/v1/auth/login')
-    api.add_resource(Logout, '/resources/v1/auth/logout')
+    api.add_resource(EventList, '/api/v1/events')
+    api.add_resource(Event, '/api/v1/events/<string:id>')
+    api.add_resource(UserList, '/api/v1/users')
+    api.add_resource(User, '/api/v1/users/<string:id>')
+    api.add_resource(Login, '/api/v1/auth/login')
+    api.add_resource(Logout, '/api/v1/auth/logout')
